0434060a67d5de8a90fa54d8e77b7f8c9541ed2e
unknown <dejanmaric259@gmail.com>
2024-08-13 22:41:46 +0200

    writing first goroutine

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var name string
+	var age int
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
+	fmt.Printf("Hello, %s!\n", name)
+	var year int = 2024 - age
+	fmt.Printf("You were born in  %d\n", year)
 }
